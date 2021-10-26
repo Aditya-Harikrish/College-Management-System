5d458b46b8856c26df8dc0d2296d3a026289b17c
PandaZGV2 <aaronmonis22@gmail.com>
2021-10-26 15:01:14 +0400

    Admin Access

--- College_Management_System.py
+++ College_Management_System.py
@@ -22,7 +22,16 @@ def admin_login(con, cur, admin_id, admin_password):
         return -1
 
 def admin(con, cur):
-    return
+    print("Edit an entity: ")
+    print("1. Student")
+    print("2. Staff")
+    print("3. Course")
+    print("4. Exit")
+    ch = int(input("Enter your choice: "))
+    if ch == 1:
+        student(con, cur)
+    elif ch == 2:
+        staff(con, cur)
 
 def user_login(con, cur, user_id, user_password):
     try: 
